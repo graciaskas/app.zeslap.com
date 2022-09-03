e5214d6261bd92853f7792a7b8267c75002bebe3
graciaskas <makaveligracias@aol.com>
2022-09-03 12:45:34 +0200

    commit one

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World...</h1>
+      <button className="btn b-blue t-white">Login</button>
+      <form action="" method="get">
+        <input type="text" placeholder="Name" className="form-control"/>
+      </form>
+    </div>
+  );
+}
+
+export default App;
